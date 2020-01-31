741c3f45c211fd9c90393c0c28cd0e2350e8f98e
Andreas G <44466249+Gillgren@users.noreply.github.com>
2020-01-31 16:46:46 +0100

    Update BurgerEQ.py

--- BurgerEQ.py
+++ BurgerEQ.py
@@ -1,4 +1,5 @@
 import sys
+import sys
 import jax.numpy as np
 from jax import random
 from jax import grad
@@ -38,12 +39,20 @@ def loss(params, x, t):
     bc3 = f_vect(params, x, t-t) - x*(1-x)
     return np.mean(eq**2) + np.mean(bc1**2) + np.mean(bc2**2) + np.mean(bc3**2)
 
+def nest_momentum(x, t, momentum, lr):
+    global params
+    global velocity
+    gradient = grad_loss(params + momentum*velocity, x, t)
+    velocity = momentum*velocity - lr*gradient
+    params += velocity
+
+
 ##########
 ## Main ##
 ##########
 
 key = random.PRNGKey(0)
-params = random.normal(key, shape=(401,))
+params = random.normal(key, shape=(401,)) * 0.07
 
 #-- Setting up the functions and derivatives --#
 
@@ -59,7 +68,7 @@ grad_loss = jit(grad(loss, 0))
 #-- Defining the domain of x, t, and bc1 --#
 
 x_values = np.linspace(0, 1, num=40)
-t_values = np.linspace(0, 10, num=40)
+t_values = np.linspace(0, 5, num=40)
 
 x = []
 t = []
@@ -74,8 +83,8 @@ t = np.asarray(t)
 
 #-- Training parameters --#
 
-epochs = 100000
-learning_rate = 0.000000001
+epochs = 500000
+learning_rate = 0.005
 momentum = 0.99
 velocity = 0.
 
@@ -84,19 +93,28 @@ velocity = 0.
 for epoch in range(epochs):
     if epoch % 100  == 0:
         print('epoch: %3d loss: %.6f' % (epoch, loss(params, x, t)))
-    if epoch == 1000:
-        learning_rate = 0.000001
-    if epoch == 10000:
-        learning_rate = 0.0001
-    gradient = grad_loss(params + momentum*velocity, x, t)
-    velocity = momentum*velocity - learning_rate*gradient
-    params += velocity
+    nest_momentum(x, t, momentum, learning_rate, velocity)
 
 #-- Plotting and comparison with analytical solution --#
 
+x_values = np.linspace(0, 1, num=100)
+t_values = np.linspace(0, 5, num=100)
+
+x = []
+t = []
+
+for i in range(len(x_values)):
+    for j in range(len(t_values)):
+        x.append(x_values[i])
+        t.append(t_values[j])
+
+x = np.asarray(x)
+t = np.asarray(t)
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.scatter3D(x, t, f_vect(params, x, t))
+plot = ax.scatter3D(x, t, f_vect(params, x, t), c = f_vect(params, x, t))
+fig.colorbar(plot)
 ax.set_xlabel('x')
 ax.set_ylabel('t')
 ax.set_zlabel('f(x,t)')
